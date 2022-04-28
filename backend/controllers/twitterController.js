62ff0747a4aef9b958ca4f396575d2efcbc6c5a9
NikDzub <nikidzub96@gmail.com>
2022-04-28 15:11:54 +0300

    getTweets res

--- backend/controllers/twitterController.js
+++ backend/controllers/twitterController.js
@@ -7,8 +7,21 @@ const getTweets = asyncHandler(async (req, res) => {
     `https://api.twitter.com/2/tweets/search/recent?max_results=10&tweet.fields=public_metrics,created_at&user.fields=entities,location&expansions=author_id&query="$${req.params.id}" -is:reply -has:mentions -is:retweet lang:en -has:links -has:media -has:images -has:hashtags`,
     { headers: header }
   );
+  let simpleData = {
+    lastDate: Date.parse(
+      tweets.data.data[tweets.data.data.length - 1].created_at
+    ),
+    totLikes: 0,
+    totUsers: tweets.data.includes.users.length,
+    totTweets: tweets.data.meta.result_count,
+    totComms: 0,
+  };
+  tweets.data.data.map((t) => {
+    simpleData.totLikes += t.public_metrics.like_count;
+    simpleData.totComms += t.public_metrics.reply_count;
+  });
 
-  res.status(200).json(tweets.data);
+  res.status(200).json(simpleData);
 });
 
 module.exports = { getTweets };
