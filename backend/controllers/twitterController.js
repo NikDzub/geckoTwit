86bb21c7c5a99d66394d740034d2830783229088
NikDzub <87159434+NikDzub@users.noreply.github.com>
2022-04-29 17:45:10 +0300

    .

--- backend/controllers/twitterController.js
+++ backend/controllers/twitterController.js
@@ -1,13 +1,18 @@
-const asyncHandler = require("express-async-handler");
-const axios = require("axios").default;
+const asyncHandler = require('express-async-handler');
+const axios = require('axios').default;
 
 const getTweets = asyncHandler(async (req, res) => {
+  //https://api.twitter.com/2/tweets/search/recent?query=%24set%20OR%20%2310set&start_time=2022-04-26T00:00:00.000Z&max_results=10&tweet.fields=text
+  const query = `query="$zpay"`;
   const header = { Authorization: `Bearer ${process.env.TWTBEAR}` };
   const tweets = await axios.get(
-    `https://api.twitter.com/2/tweets/search/recent?max_results=10&tweet.fields=public_metrics,created_at&user.fields=entities,location&expansions=author_id&query="$${req.params.id}" -is:reply -has:mentions -is:retweet lang:en -has:links -has:media -has:images -has:hashtags`,
-    { headers: header }
+    `https://api.twitter.com/2/tweets/search/recent?${query}`,
+    {
+      headers: header,
+    }
   );
-  let simpleData = {
+  let simpleData = tweets.data;
+  /* let simpleData = {
     lastDate: Date.parse(
       tweets.data.data[tweets.data.data.length - 1].created_at
     ),
@@ -19,7 +24,7 @@ const getTweets = asyncHandler(async (req, res) => {
   tweets.data.data.map((t) => {
     simpleData.totLikes += t.public_metrics.like_count;
     simpleData.totComms += t.public_metrics.reply_count;
-  });
+  }); */
 
   res.status(200).json(simpleData);
 });
